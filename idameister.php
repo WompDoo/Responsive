a8851663e9de0cda6e4ebb944bfb9332d4b34378
Heigo Kure <heigo.kure@khk.ee>
2016-01-05 18:07:04 +0200

    responsive

---
+++ idameister.php
@@ -0,0 +1,45 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    <title>IDAMEISTER</title>
+    <meta name="keywords" content="keyword1, keyword2, keyword3, etc..." />
+    <meta name="description" content="Description of website here..." />
+    <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div id="main">
+    <div id="interior">
+        <div id="top">
+            <div id="slogan">
+                <div align="left"><a href="est-index.html"><img src="images/idaremlogo.png" alt="IDAREM" border="0" /></a></div>
+            </div>
+            <div align="left"><a href="est-index.html"><img src="images/est.jpg" height="20" border="0" /></a> <a href="../index.html"><img src="images/rus.jpg" height="20" border="0" /></a></div>
+        </div>
+        <div class="clear"></div>
+        <div id="mid">
+            <div id="left">
+                <div id="navbar">
+                    <ul>
+                        <li class="button1"><a href="est-index.html">Pealeht</a></li>
+                        <li class="button2"><a href="est-about.html">Firmast</a></li>
+                        <li class="button3"><a href="est-services.html">Teenused</a></li>
+                        <li class="buttonlast"><a href="est-contact.html">Kontakt</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div id="right"><img src="images/photo-main3.jpg"/></div>
+            <div id="maintext">
+                <h1>Tere tulemast!</h1>
+                <p>Firma IDAMEISTER pakub remondi- ja ehitust&ouml;&ouml;de teenuseid nii eraisikutele kui korteri&uuml;histustele.</p>
+                <p>Professionaalsed t&ouml;&ouml;tajad, s&otilde;bralikud hinnad ja t&ouml;&ouml;garantii.</p>
+            </div>
+        </div>
+        <div class="clear"></div>
+    </div>
+</div>
+<div id="footer"> &copy;IDAMEISTER<br />
+    <a href="est-contact.html" target="_blank" style="text-decoration:none;">Tel: (+372) 58260688 | e-mail: info@idameister.ee</a> </div>
+</body>
+</html>
